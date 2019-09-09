e4b9f81cbfa9c2e2531c0871c3af9e6c55dd441b
manrajt <manrajt@umich.edu>
2019-09-09 12:18:13 -0400

    Hello world

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+import random
+
+print("Manraj Toor")
+r = random.randint(0, 100)
+print(r)
+r = random.randint(0, 100)
+print(r)
+temp = 100 + 1
+print("Sum = ", temp)
+temp = ((10+10)/2)
+print("Average = ", temp)
